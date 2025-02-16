dece8cd1a0d52ee44ffa53ca31a6000edcf82620
Anshika Rakheja <anshikarakheja@Anshikas-MacBook-Air.local>
2025-02-16 20:39:49 +0530

    Updated folder contents

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ const RestaurantCart = (props) => {
         }
       />
       <h3>{resData.info.name}</h3>
-      <h4>{resData.info.cuisines.join(",")}</h4>
+      <h4>{resData.info.cuisines.join(" , ")}</h4>
       <h4>{resData.info.avgRating}</h4>
       <h4>{resData.info.costForTwo}</h4>
       <h4>{resData.info.sla.deliveryTime}minutes</h4>
